72e1bb6699af7e139b8192a9964a35a11fa224bd
Omar Parra <omarfdez92@gmail.com>
2019-05-29 01:02:45 -0500

    Add code to value if is not empty and if it has something intent to next window. I still need to validate more the login but for today i call off

--- app/src/main/java/com/omarparra/emporioapp/MainActivity.kt
+++ app/src/main/java/com/omarparra/emporioapp/MainActivity.kt
@@ -1,9 +1,33 @@
 package com.omarparra.emporioapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    fun loginClicked(view: View){
+
+        val email = emailText.text.toString()
+        val password = passwordText.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+
+            val mainChooseIntent = Intent(this, MainChoose::class.java)
+            startActivity(mainChooseIntent)
+
+        }else{
+
+            Toast.makeText(this, "Por favor ingresa un correo y una contraseña", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
