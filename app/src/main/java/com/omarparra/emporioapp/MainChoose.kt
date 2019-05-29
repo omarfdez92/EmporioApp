dedac44c2c393b9faa22c80145c3ecffd42175f2
Omar Parra <omarfdez92@gmail.com>
2019-05-29 00:46:54 -0500

    Changes added in styles with the colors, add new activity named main choose, add montserrat font, done qith this part

---
+++ app/src/main/java/com/omarparra/emporioapp/MainChoose.kt
@@ -0,0 +1,12 @@
+package com.omarparra.emporioapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainChoose : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_choose)
+    }
+}
